7d650c11546a6ea8a54e0a4e1d6d5cc48a9455bf
alfort74 <alfort625@gmail.com>
2014-10-12 15:18:12 +0900

    Trash Commit
    Please ignore.

---
+++ fes/Assets/wallvectol.js
@@ -0,0 +1,20 @@
+﻿#pragma strict
+
+function Start () {
+
+}
+
+function Update () {
+
+}
+
+function OnTriggerEnter(collider:Collider) {
+    if (collider.gameObject.CompareTag('Player'))
+    {
+    	collider.gameObject.rigidbody.AddForce(
+        Vector3.right ,
+        ForceMode.Impulse
+    	);
+    }
+
+}
